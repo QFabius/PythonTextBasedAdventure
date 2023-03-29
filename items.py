64e52c0b2eec9ed51f96c5266e5c28b363627edb
QFabius <75182785+QFabius@users.noreply.github.com>
2023-03-29 14:22:20 +0200

    Bug-fix Item() class

--- items.py
+++ items.py
@@ -2,8 +2,8 @@
 class Item:
     # Constructor
     def __init__(self, name, description):
-        self.name
-        self.description
+        self.name = name
+        self.description = description
 
     def __str__(self):
         return "{NAME}:\n\t{DESCRIPTION}".format(NAME=self.name, DESCRIPTION=self.description)
